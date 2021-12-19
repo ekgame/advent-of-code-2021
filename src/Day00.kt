53b81e2bba4cfac0e0f6bb1d491cc015bdb56544
ekgame <ekgame1@gmail.com>
2021-12-19 13:29:58 +0200

    Added boilerplate code for upcoming days.

---
+++ src/Day00.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("day_00.example")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("day_00")
+    println(part1(input))
+    println(part2(input))
+}
